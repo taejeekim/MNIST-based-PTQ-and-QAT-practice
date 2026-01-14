9db485ea9b7cbb2cc3aeea934a6873fcc559e882
taejeekim <tjkyle14@gmail.com>
2026-01-14 10:42:47 +0900

    update

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 import time
 import os
 
